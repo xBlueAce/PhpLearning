4a78e18d3f2996b56887db73bb5b3706c221d3ff
Carlos Agustin <carlos.agustin218@gmail.com>
2021-03-24 10:58:37 -0700

    adding files functions

--- arrays.php
+++ arrays.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Arrays</title>
 </head>
 <body>
     <?php
@@ -23,6 +23,7 @@
 
     echo $numberList[4];        //display:267
     
+    say_Something();
 
     ?>  
     
